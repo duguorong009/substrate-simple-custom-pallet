b8446b1927b31f317061282a8eb20a98d3c3f0b9
duguorong <dudurong009@gmail.com>
2022-09-11 20:12:00 -0700

    Add the "custom" pallet benchmarking in runtime toml

--- pallets/custom/src/benchmarking.rs
+++ pallets/custom/src/benchmarking.rs
@@ -4,16 +4,19 @@ use super::*;
 
 #[allow(unused)]
 use crate::Pallet as Custom;
+use core::hash::Hash;
 use frame_benchmarking::{benchmarks, whitelisted_caller};
 use frame_system::RawOrigin;
 
 benchmarks! {
-	do_something {
-		let s in 0 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
+	add_member {
+		let s in 0..100;
+		// let caller: T::AccountId = whitelisted_caller();
+		let club = T::Hashing::hash("club");
+		let member = whitelisted_caller();
+	}: _(RawOrigin::Root, club, member)
 	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
+		assert_eq!(Clubs::<T>::get(club).unwrap().len(), s as usize + 1);
 	}
 
 	impl_benchmark_test_suite!(Custom, crate::mock::new_test_ext(), crate::mock::Test);
--- pallets/custom/src/lib.rs
+++ pallets/custom/src/lib.rs
@@ -17,8 +17,9 @@ mod benchmarking;
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::pallet_prelude::*;
+	use frame_support::{inherent::Vec, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
+	use scale_info::prelude::vec;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
